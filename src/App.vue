83185b0cb9eca3c3c36ed29d79271a8fb6895f04
Jessica <>
2020-12-18 23:38:00 +0800

    edit: 調整畫面

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,74 @@
 <template>
+  <Header />
   <div class="container">
-    <h1>csv檔上傳，區分語言轉換為i18n可用json格式。</h1>
-    <div class="content">
-      <h3>使用規則</h3>
-      <ul>
-        <li>A1：基本上打什麼都可以，不計入資料顯示。</li>
-        <li>B1, C1, D1, ...Z1：語言的名稱，同時也是下載json檔案的名稱。</li>
-        <li>選擇語言後點選「匯出」</li>
-      </ul>
-      <img src="csv-demo.png" width="300" />
-      <div class="result">
-        匯出的結果：<code>en.json</code>
-        <br />
-        <br />
-        <br />
+    <div class="section1">
+      <transition appear name="fadeDown">
+        <div class="section1--header">
+          <h1>CSV to JSON</h1>
+          <p>上傳指定格式csv檔，輕鬆轉換為i18n可用格式的json檔</p>
+        </div>
+      </transition>
+      <transition appear name="fade">
+        <div>
+          <label class="section1--upload">
+            上傳檔案
+            <input type="file" accept="text/csv" @change="handleChooseFile" />
+          </label>
+        </div>
+      </transition>
+      <span class="section1--filename">{{ states.csvName }}</span>
+    </div>
+
+    <div class="section2" v-if="states.csvHead.length > 0">
+      <div class="section2--table">
+        <table>
+          <thead>
+            <tr>
+              <th v-for="i in states.csvHead" :key="i">{{ i }}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="i in states.csvBody" :key="i">
+              <td v-for="j in i" :key="j">{{ j }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="section2--download">
+        <select class="section2--select" v-model="states.lang">
+          <option v-for="i in langs" :key="i.id" :value="i.id">
+            {{ i.name }}
+          </option>
+        </select>
+        <button class="section2--btn" @click="handleParseToJson">匯出</button>
+      </div>
+    </div>
+
+    <div class="section3">
+      <div class="section3--demo">
+        <img src="csv-demo.png" width="100%" />
+      </div>
+      <div class="section3--code">
+        <span class="section3--code--filename">[ en.json ]</span>
         <pre>
 {
-  "logout":"Logout",
-  "login":"Login"
-}
-      </pre
+  "logout" : "Logout",
+  "login" : "Login"
+}</pre
         >
       </div>
     </div>
-    <div class="form-group">
-      <label class="upload">
-        上傳檔案
-        <input type="file" accept="text/csv" @change="handleChooseFile" />
-      </label>
-      <span class="filename">{{ states.csvName }}</span>
-    </div>
-    <div class="form-group" v-if="states.csvHead.length > 0">
-      選擇語言：<select v-model="states.lang">
-        <option v-for="i in langs" :key="i.id" :value="i.id">{{
-          i.name
-        }}</option>
-      </select>
-      <button @click="handleParseToJson">匯出</button>
-    </div>
-    <table>
-      <thead>
-        <tr>
-          <th v-for="i in states.csvHead" :key="i">{{ i }}</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="i in states.csvBody" :key="i">
-          <td v-for="j in i" :key="j">{{ j }}</td>
-        </tr>
-      </tbody>
-    </table>
   </div>
 </template>
 
 <script>
 import { reactive, computed } from "vue";
 import Papa from "papaparse";
+import Header from "@/components/header";
 export default {
+  components: {
+    Header
+  },
   setup() {
     const states = reactive({
       csvHead: [],
@@ -110,13 +122,3 @@ export default {
   }
 };
 </script>
-<style lang="scss" scoped>
-tr th,
-tr td {
-  border: 1px solid #ccc;
-  padding: 10px 15px;
-}
-tr th {
-  background-color: #f7f7f7;
-}
-</style>
---
+++ src/components/header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="header">
+    <img src="logo.jpg" />
+    <h1>再不寫就要忘了</h1>
+  </div>
+</template>
+<script>
+export default {};
+</script>
